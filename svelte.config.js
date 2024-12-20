f470e786cfaaa591fedf035cedb2675e61beb8c9
justanormaldog-disc <129561053+justanormaldog-disc@users.noreply.github.com>
2024-12-21 01:19:53 +1100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs22.12.0',
-		  }),
+		adapter: adapter(),
 	}
 };
 
